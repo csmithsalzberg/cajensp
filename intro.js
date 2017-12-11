7627f98a4ca267c3e1c8662f4fe5ed9f58a064ec
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2017-12-11 12:17:56 -0500

    almost:

--- intro.js
+++ intro.js
@@ -8,7 +8,6 @@ HW16 -- Sequential Progression II: Electric Boogaloo
 
 var button = document.getElementById("b");
 var itemNum = 8;
-
 var addlistele = function(e){
     var list = document.getElementById("thelist"); //full list
     var newEle = document.createElement("li"); //create a new list element
@@ -49,4 +48,84 @@ while(i < listEle.length){ //add listeners
     listEle[i].addEventListener("click", remclicked);
     i++;
 }
+var linebreak = document.createElement("br");
+document.body.appendChild(linebreak);
+var linebreak = document.createElement("br");
+document.body.appendChild(linebreak);
+
+var newB = document.createElement("button"); //add a button with text
+var textB = document.createTextNode("next fib");
+newB.appendChild(textB);
+document.body.appendChild(newB);
+var newL = document.createElement("ol"); //add empty list
+document.body.appendChild(newL); 
+
+var fib = function(n){
+  if (n == 0)
+    return 0;
+  if (n <= 2)
+    return 1;
+  return fib(n-1) + fib(n-2);
+};
+
+var fibList = function(e){
+    var newEle = document.createElement("li");
+    var fibeles = newL.getElementsByTagName("li");
+    if (fibeles.length < 2)
+	var text = document.createTextNode(fibeles.length);
+    else{
+	console.log(fibeles[fibeles.length-1].innerHTML);
+	var num1 = Number(fibeles[fibeles.length-1].innerHTML);
+	var num2 = Number(fibeles[fibeles.length-2].innerHTML);
+	var text = document.createTextNode(num1+num2);
+	
+    }
+    newEle.appendChild(text);
+    newL.appendChild(newEle);
+};
+
+newB.addEventListener("click", fibList);
+
+
+
+
+var linebreak = document.createElement("br");
+document.body.appendChild(linebreak);
+var linebreak = document.createElement("br");
+document.body.appendChild(linebreak);
+
+var newB = document.createElement("button"); //add a button with text
+var textB = document.createTextNode("next fact");
+newB.appendChild(textB);
+document.body.appendChild(newB);
+var newL = document.createElement("ol"); //add empty list
+document.body.appendChild(newL); 
+
+var fib = function(n){
+  if (n == 0)
+    return 0;
+  if (n <= 2)
+    return 1;
+  return fib(n-1) + fib(n-2);
+};
+
+var counter = 0;
+var fibList = function(e){
+    var newEle = document.createElement("li");
+    var text = document.createTextNode( fib(counter) );
+    newEle.appendChild(text);
+    newL.appendChild(newEle);
+    counter++;
+
+};
+
+newB.addEventListener("click", fibList);
+
+var fact = function(n){
+  if (n < 2)
+    return 1;
+  return n * fact(n-1);
+};
+
+
 
